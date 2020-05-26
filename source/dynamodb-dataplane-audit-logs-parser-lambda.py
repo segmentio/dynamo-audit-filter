3be2b7258c8ec20c27e9cb992ccec4935edada51
Murph <57779084+murph-cat@users.noreply.github.com>
2020-05-26 14:25:51 -0400

    Fix lambda
    Make it actually work

--- source/dynamodb-dataplane-audit-logs-parser-lambda.py
+++ source/dynamodb-dataplane-audit-logs-parser-lambda.py
@@ -1,7 +1,6 @@
 import json
 import boto3
-#from io import BytesIO
-from io import StringIO
+from io import BytesIO
 from gzip import GzipFile
 import re
 
@@ -10,17 +9,16 @@ print('Loading function')
 
 s3_client = boto3.client('s3')
 kinesis_client = boto3.client('kinesis')
-# This is the prod stream.
-# stream_name = "p8t-incoming-dynamodb-dataplane-audit"
-# This is the current dev stream.
-stream_name = "dynamo-filtered-output"
+
+stream_name = "p8t-incoming-dynamodb-dataplane-audit"
+# Dev stream name:
+# stream_name = "dynamo-filtered-output"
 
 def send_event_kinesis(data):
     response = kinesis_client.put_record(
         StreamName=stream_name,
         Data=data,
-        # This should actually be unique to shard correctly.
-        PartitionKey='PartitionKey'
+        PartitionKey='PartitionKey' # TODO: make unique
     )
     return response['ResponseMetadata']['HTTPStatusCode'] == 200
 
@@ -29,21 +27,16 @@ def lambda_handler(event, context):
     key_name=event['Records'][0]['s3']['object']['key']
 
     if not "CloudTrail-Digest" in key_name:
-        obj = s3.get_object(Bucket=bucket_name,Key=key_name)
-        compressed = StringIO(obj['Body'].read())
-        decompressed = gzip.GzipFile(fileobj=compressed,mode='rb')
-        extracted = json.loads(decompressed.read())
-
-        combined_events = []
+        obj = s3_client.get_object(Bucket=bucket_name,Key=key_name)
+        bytestream = BytesIO(obj['Body'].read())
+        decompressed = GzipFile(None, 'rb', fileobj=bytestream).read().decode('utf-8')
+        extracted = json.loads(decompressed)
 
         # Look for actions done by a human user.
         for record in extracted['Records']:
             if re.search("^.*@segment.com", record["userIdentity"]["principalId"]):
-                print(entry)
-                combined_events.append(entry)
-                
-        # Send matching events to p8t.
-        if send_event_kinesis(combined_events):
-            print("data sent to kinesis: {}".format(combined_events))
-        else:
-            print("Error sending event to kinesis {}".format(combined_events))
+                print(record)
+                if send_event_kinesis(bytes(json.dumps(record), 'UTF-8')):
+                    print("data sent to kinesis: {}".format(record))
+                else:
+                    print("Error sending event to kinesis {}".format(record))
